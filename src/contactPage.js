c94ced4742e6665985560a3783bcb16061a79af3
jakenlightbringer <mememe49bf4@gmail.com>
2023-09-10 23:37:20 -0700

    completed contact information, menu next

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,20 +1,76 @@
 function contactPage() {
     const contentContainer = document.getElementById('content');
+
+    const tableContainer = document.createElement('div');
+    tableContainer.classList.add('table-container');
+    const hoursTable = document.createElement('table');
+    hoursTable.classList.add('hours-table');
+    hoursTable.innerHTML = `
+        <caption>Hours of Operation</caption>
+        <thead>
+            <tr>
+                <th>Day</th>
+                <th>Opening Hours</th>
+                <th>Closing Hours</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Monday</td>
+                <td>9:00 AM</td>
+                <td>8:00 PM</td>
+            </tr>
+            <tr>
+                <td>Tuesday</td>
+                <td>9:00 AM</td>
+                <td>8:00 PM</td>
+            </tr>
+            <tr>
+                <td>Wednesday</td>
+                <td>9:00 AM</td>
+                <td>8:00 PM</td>
+            </tr>
+            <tr>
+                <td>Thursday</td>
+                <td>9:00 AM</td>
+                <td>8:00 PM</td>
+            </tr>
+            <tr>
+                <td>Friday</td>
+                <td>9:00 AM</td>
+                <td>8:00 PM</td>
+            </tr>
+            <tr>
+                <td>Saturday</td>
+                <td>10:00 AM</td>
+                <td>8:00 PM</td>
+            </tr>
+            <tr>
+                <td>Sunday</td>
+                <td>Closed</td>
+                <td>Closed</td>
+            </tr>
+        </tbody>
+    `;
+
   
     const contactHeader = document.createElement('div');
-    contactHeader.textContent = 'Contact Information'; 
+    contactHeader.textContent = 'Contact Information';
+    contactHeader.classList.add('contact-header');
   
     const contactInformation = document.createElement('div');
-    contactInformation.textContent = 'Phone Number: 132-332-3412\nAddress: 123 Street, City, State, Zipcode'; 
+    contactInformation.innerHTML = `
+        <p>Phone Number: 132-332-3412</p>
+        <br>
+        <p>Address: 123 Street, City, State, Zipcode</p>
+    `;
     contactInformation.classList.add('contact-information');
   
-    const hoursInformation = document.createElement('div');
-    hoursInformation.textContent = 'Hours of Operation:\nMonday - Friday 11am-9pm\nSat-Sun 11am - 11pm'; 
-    hoursInformation.classList.add('hours-information');
-  
+    tableContainer.appendChild(hoursTable)
+    contentContainer.appendChild(tableContainer);
     contentContainer.appendChild(contactHeader);
     contentContainer.appendChild(contactInformation);
-    contentContainer.appendChild(hoursInformation);
+    
   }
   
   export default contactPage;
